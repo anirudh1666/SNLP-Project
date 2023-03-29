fa57bc949f63db5a8170f867b91e52fa8c66376e
Yuwei Zhu <64314689+yuwei-1@users.noreply.github.com>
2023-03-29 14:56:57 +0100

    added decoder only architecture

--- src/utils/transformer/decoder.py
+++ src/utils/transformer/decoder.py
@@ -26,3 +26,27 @@ class Decoder(nn.Module):
     x2 = self._dropout2(self._src_attn(x, encoder_output, encoder_output, src_mask))
     x = self._norm2(x + x2)
     return self._norm3(x + self._ff(x))
+  
+class DecoderNE(nn.Module):
+  def __init__(self, d_model, h, d_ff, dropout, compressed=False):
+    super().__init__()
+    if not compressed:
+      self._self_attn_1 = MultiHeadedAttention(h, d_model)
+      self._self_attn_2 = MultiHeadedAttention(h, d_model)
+    else:
+      self._self_attn_1 = MemoryCompressedAttention(h, d_model)
+      self._self_attn_2 = MemoryCompressedAttention(h, d_model)
+      
+    self._ff = FeedForward(d_model, d_ff, dropout)
+    self._norm1 = nn.LayerNorm(d_model)
+    self._norm2 = nn.LayerNorm(d_model)
+    self._norm3 = nn.LayerNorm(d_model)
+    self._dropout1 = nn.Dropout(dropout)
+    self._dropout2 = nn.Dropout(dropout)
+
+  def forward(self, x, tgt_mask, src_mask=None):
+    x2 = self._dropout1(self._self_attn_1(x, x, x, tgt_mask))
+    x = self._norm1(x + x2)
+    x2 = self._dropout2(self._self_attn_2(x, x, x, src_mask))  # does this need mask?
+    x = self._norm2(x + x2)
+    return self._norm3(x + self._ff(x))
