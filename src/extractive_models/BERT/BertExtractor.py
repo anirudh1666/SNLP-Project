e2e1a1a4d4abfebbbb6d360e7d97ec75632fa978
JulianPJ <56226778+JulianPJ@users.noreply.github.com>
2023-03-30 14:02:57 +0100

    Changed how bert is accessed
    Changed bert's method to access from specifying articles to extract to passing in the text which you want to extract. Can be newline-separated text.

--- src/extractive_models/BERT/BertExtractor.py
+++ src/extractive_models/BERT/BertExtractor.py
@@ -1,27 +1,19 @@
-from ExtractArticles import getArticles
 from summarizer import Summarizer
-
 """
 Uses pretrained bert extractor from https://github.com/dmmiller612/bert-extractive-summarizer.
-Define the file path of the document you want to read through. 
-Additionally define article to read, default is 0.
+Define the text to summarize.
 Can also define number of sentences/ratio of original to extract (Optional).
-Returns the summarized text text and the original text.
+Returns the summarized text
 Returns:
-    (string, string): Tuple in format (summary, original)
+    string: String of summary
 """
-def getBertSummarization(filepath, article=0, num_sentences=-1, ratio=-1):
-    articles = getArticles(filepath, article, 1) # Get 1 article from file
-    article1 = ' '.join(articles[0]) # Articles in list of list, so get first one and make it a single string
+def getBertSummarization(text, num_sentences=-1, ratio=-1):
 
     bert = Summarizer() # Initialize bert summarizer, example of adding hidden layers : Summarizer(hidden=[-1,-2], hidden_concat=True)
     
     # Get bert's predicted summarization, using length if specified
     if num_sentences != -1:
-        out = bert(article1, num_sentences=num_sentences) 
-    elif ratio != -1:
-        out = bert(article1, ratio=ratio)
-    else:
-        out = bert(article1)
-    
-    return (out, article1) # Return (summary, original) tuple
+        return bert(text, num_sentences=num_sentences) 
+    if ratio != -1:
+        return bert(text, ratio=ratio)
+    return bert(text)
