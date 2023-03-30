fixed compressed masking

src/utils/transformer/mh_compressed_attention.py
@@ -30,7 +30,7 @@ class MemoryCompressedAttention(nn.Module):
 
     def _compress_sub_mask(self, seq_length, comp_length, cf):
         subsequent_mask = np.triu(np.ones((comp_length, comp_length)), k=1).astype('uint8')
-        subsequent_mask = np.stack(itertools.chain.from_iterable([[m]*cf for m in subsequent_mask]))
+        subsequent_mask = np.stack(list(itertools.chain.from_iterable([[m]*cf for m in subsequent_mask])))
         trim = comp_length*cf - seq_length
         return (torch.from_numpy(subsequent_mask) == 0)[:-trim]
 
@@ -46,7 +46,7 @@ class MemoryCompressedAttention(nn.Module):
 
         key, value = map(self._memcomp, (key, value))
         compressed_size = key.shape[-2]
-        mask = self._compress_sub_mask(l, compressed_size, l//compressed_size + 1)
+        mask = self._compress_sub_mask(l, compressed_size, l//compressed_size + 1).unsqueeze(0)
 
         # 2) Do all the linear projections in batch from d_model => h x d_k 
         query, key, value = \